e20798dadf10c62d69328cf251fea3cf8b0df259
Eric <bostrot@users.noreply.github.com>
2018-02-28 20:33:48 +0100

    Testing

--- bin/support.js
+++ bin/support.js
@@ -217,6 +217,7 @@ bot.on("photo", downloadPhotoMiddleware, (ctx, next) => { // send any received p
 
 bot.hears(/(.+)/, (ctx) => { // creates a ticket for users and let group admins in staff_chat reply to those
   ctx.getChat().then(function(chat) {
+    console.log("chat id "+ chat.id+"staff id "+ staff_chat)
     if (chat.id === staff_chat) {
       console.log("chat id "+ chat.id)
       ctx.getChatAdministrators().then(function(admins) { // reply to users ticket
